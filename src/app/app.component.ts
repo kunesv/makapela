373fc39c1b443224ab304701dc037e5c052ba360
kunesv <kunesv@sprinters.cz>
2022-11-19 22:23:49 +0100

    Drag and drop songs, implemented following Google Dev guide at Building web app wint Angular and Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { Song } from './song/song';
 import { Component } from '@angular/core';
+
+import { CdkDragDrop, transferArrayItem, moveItemInArray } from "@angular/cdk/drag-drop";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,159 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'makapela';
+  songs: Song[] = [
+    {
+      title: 'Ohne Dich',
+      text: `
+Dmi Ami Gmi F Ami
+
+1:
+Dmi                 Ami
+   Ich werde in die tannen gehen
+Gmi                F       Ami
+Dahin wo ich sie zuletzt gesehn
+Dmi                         Ami
+   Doch der Abend wirft ein Tuch aufs Land
+Gmi                       F     Ami
+ Und auf die Wege hinterm Waldesrand
+Dmi                         Ami
+   Und der Wald er steht so schwartz und leer
+Gmi
+Weh mir of weh
+F              Ami
+ Und die Vögel signen nich mehr
+
+R:
+Dmi                            Ami
+Ohne dich kann ich nicht sein, ohne dich
+Gmi                     F         Ami
+Mit dir bin ich allein, Ohne dich
+Dmi                              Ami
+Ohne dich zählt ich die Stunden, Ohne dich
+Gmi                          F            Ami
+Mit dir stehen die Sekunden, lohnen nicht
+
+2:
+Dmi                     Ami
+   Auf den Ästen in den Gräben
+Gmi                       F Ami
+Ist es nun Still und ohne Leben
+Dmi                      Ami
+ Und das Atmen fällt mir ach so schwer
+Gmi
+ Weh mir oh weh
+F              Ami
+ Und die Vögel signen nich mehr
+
+R:
+Dmi                            Ami
+Ohne dich kann ich nicht sein, ohne dich
+Gmi                     F         Ami
+Mit dir bin ich allein, Ohne dich
+Dmi                              Ami
+Ohne dich zählt ich die Stunden, Ohne dich
+Gmi                          F            Ami
+Mit dir stehen die Sekunden, lohnen nicht
+
+B:
+Dmi                      Ami
+ Und das Atmen fällt mir ach so schwer
+Gmi
+ Weh mir oh weh
+F              Ami
+ Und die Vögel signen nich mehr
+
+R:
+Dmi                            Ami
+Ohne dich kann ich nicht sein, ohne dich
+Gmi                     F         Ami
+Mit dir bin ich allein, Ohne dich
+Dmi                              Ami
+Ohne dich zählt ich die Stunden, Ohne dich
+Gmi                          F             Ami
+Mit dir stehen die Sekunden, lohnen nicht, Ohne dich
+
+...
+      `
+    },
+    {
+      title: 'První píseň',
+      text: `
+1:
+    Ami        E              Ami
+Tak jako se Ti stejská po tom lese,
+    Ami          E              Ami
+tak holce tvý se stejská po tom plese.
+     G         D           G
+Když tancovala měla přes míru
+  G          D           G
+a poblila si tu svou kanýru.
+
+R:
+ D           G         c
+Ona řekla že jí to nevadí,
+D            G          C
+že jí stejně někdo pohladí.
+
+Ami E Ami
+Ami E Ami
+
+2:
+    Ami        E              Ami
+Tak jako lyška mládě v zubech nese,
+    Ami           E            Ami
+tak brtník medvěd někde v lese rve se.
+      G              D         G
+S tou holkou která v bílym baráku,
+G        D          G
+porodila dítě v roláku.
+
+R:
+ D           G         C
+Ona řekla že jí to nevadí,
+D            G          C
+že jí stejně někdo pohladí.
+
+Ami E Ami
+Ami E Ami
+
+3:
+    Ami          E         Ami
+Jak smrtka hlavy nelítosně kosí,
+    Ami          E            Ami
+tak nad krajinou kroužej roje vosí.
+  G            D         G
+A slunce k západu se obrací,
+  G           D             G
+a tahle holka splývá s matrací.
+
+R:
+ D           G         C
+Ona řekla že jí to nevadí,
+D            G          C
+že jí stejně někdo pohladí.
+
+Ami E Ami
+Ami E
+Ami E
+Ami E Ami
+      `
+    }
+  ]
+
+  play: Song[] = []
+
+  drop(event: CdkDragDrop<Song[]>): void {
+    if (!event.container.data || !event.previousContainer.data) {
+      return
+    }
+
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex)
+    } else {
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex)
+    }
+
+    console.log(this.songs.map((item) => item.text), this.play.map((item) => item.text))
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon'
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+
+import { DragDropModule } from "@angular/cdk/drag-drop";
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SongComponent } from './song/song.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SongComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+
+    DragDropModule,
+
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
